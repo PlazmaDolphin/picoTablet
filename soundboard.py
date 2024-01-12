e9ec86a482196d2b83edcf246e1e9e555447041c
PlazmaDolphin <plasmadolphin0@gmail.com>
2024-01-11 23:37:45 -0500

    added keypad controls

--- soundboard.py
+++ soundboard.py
@@ -13,22 +13,38 @@ storage.mount(vfs, '/sd')
 
 pad = core.numpad([board.GP0, board.GP1, board.GP4, board.GP5],[board.GP7, board.GP27, board.GP26])
 
-# The listed mp3files will be played in order
 mp3files = ["/sd/bone.mp3", "/sd/boom.mp3", "/sd/awp.mp3"]
 
 # You have to specify some mp3 file when creating the decoder
 mp3 = open(mp3files[0], "rb")
 decoder = MP3Decoder(mp3)
 audio = AudioOut(board.GP28)
-
+play = False
 while True:
-    for filename in mp3files:
-        # Updating the .file property of the existing decoder
-        # helps avoid running out of memory (MemoryError exception)
-        decoder.file = open(filename, "rb")
+    if not audio.playing and play: #sfx just ended
+        audio.stop()
+        print("stopped")
+        play=False
+    key = pad.checkpressed()
+    if len(key) > 0:
+        key = key[0]
+    else:
+        continue
+    if key <= 3:
+        decoder.file = open(mp3files[key-1], "rb")
         audio.play(decoder)
-        print("playing", filename)
-
-        # This allows you to do other things while the audio plays!
-        while audio.playing:
-            pass
+        print("playing", mp3files[key-1])
+        play = True
+    if key == 4: #pause/resume
+        if play:
+            audio.pause()
+            print("paused")
+            play=False
+        else:
+            audio.resume()
+            print("resumed")
+            play=True
+    if key == 5: #stop
+        audio.stop()
+        print("stopped")
+        play=False
